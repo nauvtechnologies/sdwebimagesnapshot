a84185bc2a7634701859571fe8cc07ae06067427
Zack <ngangazack@nauv.com>
2025-01-22 11:14:51 +0300

    Initial Commit

---
+++ SDWebImageSnapshot/SDWebImageSnapshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  SDWebImageSnapshotApp.swift
+//  SDWebImageSnapshot
+//
+//  Created by Zack on 22/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct SDWebImageSnapshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SDWebImageSnapshotTests/SDWebImageSnapshotTests.swift
@@ -0,0 +1,17 @@
+//
+//  SDWebImageSnapshotTests.swift
+//  SDWebImageSnapshotTests
+//
+//  Created by Zack on 22/01/2025.
+//
+
+import Testing
+@testable import SDWebImageSnapshot
+
+struct SDWebImageSnapshotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
